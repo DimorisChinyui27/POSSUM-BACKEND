22a38374e1efceb4ae9664947ad7fab72a00321d
umagloire <umagloire99@gmail.com>
2022-11-30 17:28:02 +0100

    fixing errors

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::group([
         ], function () {
             // USERS
             Route::get('users/edit', [UserController::class, 'edit']);
-            Route::post('users/topics/remove'. [UserController::class, 'removeTopic']);
+            Route::post('users/topics/remove', [UserController::class, 'removeTopic']);
             Route::post('users/topics/add', [UserController::class, 'addTopic']);
             Route::post('users/update', [UserController::class, 'update']);
             Route::get('users/{username}/answers', [UserController::class, 'getAnswers']);
